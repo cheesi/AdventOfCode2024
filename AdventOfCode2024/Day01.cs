8c37610e7defbbeaef790918a7114c20d49322c4
Christian Schabetsberger <christian.schabetsberger@finxit.at>
2024-11-30 18:30:10 +0100

    feat: initial setup

---
+++ AdventOfCode2024/Day01.cs
@@ -0,0 +1,27 @@
+﻿
+namespace AdventOfCode2024;
+
+public class Day01 : BaseDay
+{
+    private readonly string _input;
+
+    public Day01()
+    {
+        _input = File.ReadAllText(InputFilePath);
+    }
+
+    public Day01(string input)
+    {
+        _input = input;
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Advent of Code 2024
+
+**Disclaimer**: Code looks like an absolute mess. Only solved, not refactored, nor optimized.
+
+Just sharing my solutions.
