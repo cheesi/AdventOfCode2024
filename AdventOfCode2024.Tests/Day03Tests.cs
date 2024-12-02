56cbf1e16e23125238a4ed6490715ffa06741a53
Christian Schabetsberger <christian.schabetsberger@finxit.at>
2024-12-02 09:00:55 +0100

    Day 3 preperation

---
+++ AdventOfCode2024.Tests/Day03Tests.cs
@@ -0,0 +1,37 @@
+﻿namespace AdventOfCode2024.Tests;
+
+public class Day03Tests
+{
+    [Fact]
+    public async Task Part1()
+    {
+        // Arrange
+        var input = """
+
+                    """;
+        var systemUnderTest = new Day03(input);
+
+        // Act
+        var result = await systemUnderTest.Solve_1();
+
+        // Assert
+        result.Should().Be("");
+    }
+
+
+    [Fact]
+    public async Task Part2()
+    {
+        // Arrange
+        var input = """
+
+                    """;
+        var systemUnderTest = new Day03(input);
+
+        // Act
+        var result = await systemUnderTest.Solve_2();
+
+        // Assert
+        result.Should().Be("");
+    }
+}
