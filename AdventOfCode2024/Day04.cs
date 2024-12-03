a061427256b367fa0abeee2eae181e0313da35a8
Christian Schabetsberger <christian.schabetsberger@finxit.at>
2024-12-03 09:43:04 +0100

    Day 4 preparation

---
+++ AdventOfCode2024/Day04.cs
@@ -0,0 +1,26 @@
+﻿namespace AdventOfCode2024;
+
+public class Day04 : BaseDay
+{
+    private readonly string _input;
+
+    public Day04()
+    {
+        _input = File.ReadAllText(InputFilePath);
+    }
+
+    public Day04(string input)
+    {
+        _input = input;
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        throw new NotImplementedException();
+    }
+}
