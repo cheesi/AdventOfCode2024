36aa0d99f1d02279e473a7374c1997f38c909993
Christian Schabetsberger <christian.schabetsberger@finxit.at>
2024-12-07 19:30:34 +0100

    Day 5 part 2

--- AdventOfCode2024.Tests/Day05Tests.cs
+++ AdventOfCode2024.Tests/Day05Tests.cs
@@ -28,7 +28,7 @@ public class Day05Tests
                     47|29
                     75|13
                     53|13
-                    
+
                     75,47,61,53,29
                     97,61,53,29,13
                     75,29,13
@@ -51,7 +51,34 @@ public class Day05Tests
     {
         // Arrange
         var input = """
+                    47|53
+                    97|13
+                    97|61
+                    97|47
+                    75|29
+                    61|13
+                    75|53
+                    29|13
+                    97|29
+                    53|29
+                    61|53
+                    97|53
+                    61|29
+                    47|13
+                    75|47
+                    97|75
+                    47|61
+                    75|61
+                    47|29
+                    75|13
+                    53|13
 
+                    75,47,61,53,29
+                    97,61,53,29,13
+                    75,29,13
+                    75,97,47,61,53
+                    61,13,29
+                    97,13,75,29,47
                     """;
         var systemUnderTest = new Day05(input);
 
@@ -59,6 +86,6 @@ public class Day05Tests
         var result = await systemUnderTest.Solve_2();
 
         // Assert
-        result.Should().Be("");
+        result.Should().Be("123");
     }
 }
--- AdventOfCode2024/Day05.cs
+++ AdventOfCode2024/Day05.cs
@@ -70,7 +70,7 @@ public class Day05 : BaseDay
                     foreach (var rule in applicableAfterRules)
                     {
                         var indexOf = manual.IndexOf(rule);
-                        if (indexOf != -1 && page != rule &&indexOf > indexOfPage)
+                        if (indexOf != -1 && page != rule && indexOf > indexOfPage)
                         {
                             inOrder = false;
                         }
@@ -90,6 +90,114 @@ public class Day05 : BaseDay
 
     public override ValueTask<string> Solve_2()
     {
-        throw new NotImplementedException();
+        var middlePageNumbers = new List<int>();
+        var rulesBefore = new Dictionary<int, List<int>>();
+        var rulesAfter = new Dictionary<int, List<int>>();
+        var manuals = new List<List<int>>();
+
+        using var stringReader = new StringReader(_input);
+        while (stringReader.ReadLine() is { } line)
+        {
+            if (line.Contains('|'))
+            {
+                var parts = line.Split("|").Select(int.Parse).ToArray();
+                if (!rulesBefore.ContainsKey(parts[0]))
+                {
+                    rulesBefore[parts[0]] = new List<int>();
+                }
+                rulesBefore[parts[0]].Add(parts[1]);
+
+                if (!rulesAfter.ContainsKey(parts[1]))
+                {
+                    rulesAfter[parts[1]] = new List<int>();
+                }
+                rulesAfter[parts[1]].Add(parts[0]);
+            }
+            else if (line.Contains(','))
+            {
+                var manual = line.Split(",").Select(int.Parse).ToList();
+                manuals.Add(manual);
+            }
+        }
+
+        foreach (var manual in manuals)
+        {
+            var inOrder = true;
+            foreach (var page in manual)
+            {
+                var indexOfPage = manual.IndexOf(page);
+
+                if (rulesBefore.TryGetValue(page, out var applicableBeforeRules))
+                {
+                    foreach (var rule in applicableBeforeRules)
+                    {
+                        var indexOf = manual.IndexOf(rule);
+                        if (indexOf != -1 && page != rule && indexOf < indexOfPage)
+                        {
+                            inOrder = false;
+                        }
+                    }
+                }
+
+                if (rulesAfter.TryGetValue(page, out var applicableAfterRules))
+                {
+                    foreach (var rule in applicableAfterRules)
+                    {
+                        var indexOf = manual.IndexOf(rule);
+                        if (indexOf != -1 && page != rule && indexOf > indexOfPage)
+                        {
+                            inOrder = false;
+                        }
+                    }
+                }
+            }
+
+            if (!inOrder)
+            {
+                manual.Sort(new PageSorter(rulesBefore, rulesAfter));
+                var middlePage = manual.Skip((manual.Count - 1) / 2).First();
+                middlePageNumbers.Add(middlePage);
+            }
+        }
+
+        return new ValueTask<string>(middlePageNumbers.Sum().ToString());
+    }
+
+    public class PageSorter : IComparer<int>
+    {
+        private readonly Dictionary<int, List<int>> _rulesBefore;
+        private readonly Dictionary<int, List<int>> _rulesAfter;
+
+        public PageSorter(Dictionary<int, List<int>> rulesBefore, Dictionary<int, List<int>> rulesAfter)
+        {
+            _rulesBefore = rulesBefore;
+            _rulesAfter = rulesAfter;
+        }
+
+        public int Compare(int x, int y)
+        {
+            if (_rulesBefore.TryGetValue(x, out var rulesBeforeX)
+                && rulesBeforeX.Contains(y))
+            {
+                return -1;
+            }
+            else if (_rulesBefore.TryGetValue(y, out var rulesBeforeY)
+                && rulesBeforeY.Contains(x))
+            {
+                return 1;
+            }
+            else if (_rulesAfter.TryGetValue(x, out var rulesAfterX)
+                && rulesAfterX.Contains(y))
+            {
+                return -1;
+            }
+            else if (_rulesAfter.TryGetValue(y, out var rulesAfterY)
+                && rulesAfterY.Contains(x))
+            {
+                return 1;
+            }
+
+            return 0;
+        }
     }
 }
