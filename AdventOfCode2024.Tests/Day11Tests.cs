af48dd6dfb5ec8250f421b0b369d802bdea2ccf0
Christian Schabetsberger <christian.schabetsberger@finxit.at>
2024-12-13 15:50:14 +0100

    Day 11 preparation

---
+++ AdventOfCode2024.Tests/Day11Tests.cs
@@ -0,0 +1,36 @@
+﻿namespace AdventOfCode2024.Tests;
+
+public class Day11Tests
+{
+    [Fact]
+    public async Task Part1()
+    {
+        // Arrange
+        var input = """
+
+                    """;
+        var systemUnderTest = new Day11(input);
+
+        // Act
+        var result = await systemUnderTest.Solve_1();
+
+        // Assert
+        result.Should().Be("36");
+    }
+
+    [Fact]
+    public async Task Part2()
+    {
+        // Arrange
+        var input = """
+
+                    """;
+        var systemUnderTest = new Day11(input);
+
+        // Act
+        var result = await systemUnderTest.Solve_2();
+
+        // Assert
+        result.Should().Be("");
+    }
+}
---
+++ AdventOfCode2024/Day11.cs
@@ -0,0 +1,25 @@
+﻿namespace AdventOfCode2024;
+
+public class Day11 : BaseDay
+{
+    private readonly string _input;
+
+    public Day11()
+    {
+        _input = File.ReadAllText(InputFilePath);
+    }
+
+    public Day11(string input)
+    {
+        _input = input;
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        throw new NotImplementedException();
+    }
+    public override ValueTask<string> Solve_2()
+    {
+        throw new NotImplementedException();
+    }
+}
