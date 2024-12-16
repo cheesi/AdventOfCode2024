e7370790f035ed474e58d9d556aeffaa258bee2d
Christian Schabetsberger <christian.schabetsberger@finxit.at>
2024-12-16 02:56:59 +0100

    Day 15 part 1

--- AdventOfCode2024.Tests/Day15Tests.cs
+++ AdventOfCode2024.Tests/Day15Tests.cs
@@ -3,17 +3,65 @@
 public class Day15Tests
 {
     [Fact]
-    public async Task Part1()
+    public async Task Part1_1()
     {
         // Arrange
         var input = """
+                    ########
+                    #..O.O.#
+                    ##@.O..#
+                    #...O..#
+                    #.#.O..#
+                    #...O..#
+                    #......#
+                    ########
 
+                    <^^>>>vv<v>>v<<
                     """;
         var systemUnderTest = new Day15(input);
 
         // Act
         var result = await systemUnderTest.Solve_1();
 
+        // Assert
+        result.Should().Be("2028");
+    }
+
+    [Fact]
+    public async Task Part1_2()
+    {
+        // Arrange
+        var input = """
+                    ##########
+                    #..O..O.O#
+                    #......O.#
+                    #.OO..O.O#
+                    #..O@..O.#
+                    #O#..O...#
+                    #O..O..O.#
+                    #.OO.O.OO#
+                    #....O...#
+                    ##########
+
+                    <vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+                    vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+                    ><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+                    <<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+                    ^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+                    ^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+                    >^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+                    <><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+                    ^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+                    v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^
+                    """;
+        var systemUnderTest = new Day15(input);
+
+        // Act
+        var result = await systemUnderTest.Solve_1();
+
+        // Assert
+        result.Should().Be("10092");
+    }
         // Assert
         result.Should().Be("");
     }
--- AdventOfCode2024/Day15.cs
+++ AdventOfCode2024/Day15.cs
@@ -1,4 +1,8 @@
-﻿namespace AdventOfCode2024;
+﻿using System.Diagnostics;
+using System.Drawing;
+using System.Text;
+
+namespace AdventOfCode2024;
 
 public class Day15 : BaseDay
 {
@@ -16,7 +20,121 @@ public class Day15 : BaseDay
 
     public override ValueTask<string> Solve_1()
     {
-        throw new NotImplementedException();
+        var movements = new StringBuilder();
+        var lines = _input.Split(['\r', '\n'], StringSplitOptions.RemoveEmptyEntries);
+        var lastLine = Array.FindLastIndex(lines, x => x.StartsWith("########"));
+        var map = new char[lines.Length, lines[lastLine].Length];
+        var positionRobot = new Point(0, 0);
+        for (int i = 0; i < lines.Length; i++)
+        {
+            if (i > lastLine)
+            {
+                movements.Append(lines[i]);
+            }
+            else if (i <= lastLine)
+            {
+                for (int j = 0; j < lines[i].Length; j++)
+                {
+                    map[i, j] = lines[i][j];
+                    if (map[i, j] == '@')
+                    {
+                        positionRobot = new Point(i, j);
+                    }
+                }
+            }
+        }
+        var movementInstruction = movements.ToString().ToCharArray();
+
+        //Print(map, 'I');
+
+        foreach (var movement in movementInstruction)
+        {
+            positionRobot = Move(map, positionRobot, movement);
+            //Print(map, movement);
+        }
+
+        var gpsCoordinates = new List<long>();
+        for (int i = 0; i < map.GetLength(0); i++)
+        {
+            for (int j = 0; j < map.GetLength(1); j++)
+            {
+                if (map[i, j] == 'O')
+                {
+                    gpsCoordinates.Add(100 * i + j);
+                }
+            }
+        }
+
+        return new ValueTask<string>(gpsCoordinates.Sum().ToString());
+    }
+
+    private void Print(char[,] map, char movement)
+    {
+        Debug.WriteLine(string.Empty);
+        Debug.WriteLine($"Move {movement}:");
+        for (int i = 0; i < map.GetLength(0); i++)
+        {
+            for (int j = 0; j < map.GetLength(1); j++)
+            {
+                Debug.Write(map[i, j]);
+            }
+            Debug.WriteLine(string.Empty);
+        }
+    }
+
+    private Point Move(char[,] map, Point position, char movement)
+    {
+        var newX = position.X;
+        var newY = position.Y;
+
+        switch (movement)
+        {
+            case '^':
+                newX--;
+                break;
+            case '>':
+                newY++;
+                break;
+            case 'v':
+                newX++;
+                break;
+            case '<':
+                newY--;
+                break;
+        }
+
+        if (newX >= 0
+            && newX < map.GetLength(0)
+            && newY >= 0
+            && newY < map.GetLength(1))
+        {
+            var currentTile = map[position.X, position.Y];
+            var nextTile = map[newX, newY];
+            if (nextTile == '.')
+            {
+                map[newX, newY] = currentTile;
+                map[position.X, position.Y] = '.';
+                return new Point(newX, newY);
+            }
+            else if (nextTile == 'O')
+            {
+                Move(map, new Point(newX, newY), movement);
+                nextTile = map[newX, newY];
+                if (nextTile == '.')
+                {
+                    map[newX, newY] = currentTile;
+                    map[position.X, position.Y] = '.';
+                    return new Point(newX, newY);
+                }
+            }
+            else if (nextTile == '#')
+            {
+                // path blocked
+                return position;
+            }
+        }
+
+        return position;
     }
 
     public override ValueTask<string> Solve_2()
