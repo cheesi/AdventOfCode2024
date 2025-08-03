fba04cb2d324b9c8de57b147968e616ed9dcfb63
Christian Schabetsberger <christian.schabetsberger@finxit.at>
2025-08-03 05:57:27 +0200

    Day 15 part 2

--- AdventOfCode2024.Tests/Day15Tests.cs
+++ AdventOfCode2024.Tests/Day15Tests.cs
@@ -62,16 +62,21 @@ public class Day15Tests
         // Assert
         result.Should().Be("10092");
     }
-        // Assert
-        result.Should().Be("");
-    }
 
     [Fact]
-    public async Task Part2()
+    public async Task Part2_1()
     {
         // Arrange
         var input = """
+                    #######
+                    #...#.#
+                    #.....#
+                    #..OO@#
+                    #..O..#
+                    #.....#
+                    #######
 
+                    <vv<<^^<<^^
                     """;
         var systemUnderTest = new Day15(input);
 
@@ -79,6 +84,42 @@ public class Day15Tests
         var result = await systemUnderTest.Solve_2();
 
         // Assert
-        result.Should().Be("");
+        result.Should().Be("618");
+    }
+
+    [Fact]
+    public async Task Part2()
+    {
+        // Arrange
+        var input = """
+                    ##########
+                    #..O..O.O#
+                    #......O.#
+                    #.OO..O.O#
+                    #..O@..O.#
+                    #O#..O...#
+                    #O..O..O.#
+                    #.OO.O.OO#
+                    #....O...#
+                    ##########
+                    
+                    <vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+                    vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+                    ><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+                    <<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+                    ^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+                    ^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+                    >^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+                    <><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+                    ^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+                    v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^
+                    """;
+        var systemUnderTest = new Day15(input);
+
+        // Act
+        var result = await systemUnderTest.Solve_2();
+
+        // Assert
+        result.Should().Be("9021");
     }
 }
--- AdventOfCode2024/Day15.cs
+++ AdventOfCode2024/Day15.cs
@@ -139,6 +139,220 @@ public class Day15 : BaseDay
 
     public override ValueTask<string> Solve_2()
     {
-        throw new NotImplementedException();
+        var movements = new StringBuilder();
+        var lines = _input.Split(['\r', '\n'], StringSplitOptions.RemoveEmptyEntries);
+        var lastLine = Array.FindLastIndex(lines, x => x.StartsWith("#######"));
+        var map = new char[lastLine + 1, lines[lastLine].Length * 2];
+        var positionRobot = new Point(0, 0);
+        for (int i = 0; i < lines.Length; i++)
+        {
+            if (i > lastLine)
+            {
+                movements.Append(lines[i]);
+            }
+            else if (i <= lastLine)
+            {
+                // Build map
+                for (int j = 0; j < lines[i].Length * 2; j = j + 2)
+                {
+                    var tile = lines[i][j / 2];
+                    map[i, j] = tile;
+                    if (tile == 'O')
+                    {
+                        map[i, j] = '[';
+                    }
+                    map[i, j + 1] = tile switch
+                    {
+                        '#' => '#',
+                        'O' => ']',
+                        '.' => '.',
+                        '@' => '.'
+                    };
+                    if (map[i, j] == '@')
+                    {
+                        positionRobot = new Point(i, j);
+                    }
+                }
+            }
+        }
+        var movementInstruction = movements.ToString().ToCharArray();
+
+        //Print(map, 'I');
+
+        foreach (var movement in movementInstruction)
+        {
+            positionRobot = Move2(map, positionRobot, movement);
+            //Print(map, movement);
+        }
+
+        //Print(map, 'I');
+
+        var gpsCoordinates = new List<long>();
+        for (int i = 0; i < map.GetLength(0); i++)
+        {
+            for (int j = 0; j < map.GetLength(1); j++)
+            {
+                if (map[i, j] == '[')
+                {
+                    gpsCoordinates.Add(100 * i + j);
+                }
+            }
+        }
+
+        return new ValueTask<string>(gpsCoordinates.Sum().ToString());
+    }
+
+    private bool CheckMapForInvalidTiles(char[,] map)
+    {
+        try
+        {
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    if (map[i, j] == '[')
+                    {
+                        if (map[i, j + 1] != ']')
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+
+        return true;
+    }
+
+    private Point Move2(char[,] map, Point position, char movement)
+    {
+        var newX = position.X;
+        var newY = position.Y;
+
+        switch (movement)
+        {
+            case '^':
+                newX--;
+                break;
+            case '>':
+                newY++;
+                break;
+            case 'v':
+                newX++;
+                break;
+            case '<':
+                newY--;
+                break;
+        }
+
+        if (newX >= 0
+            && newX < map.GetLength(0)
+            && newY >= 0
+            && newY < map.GetLength(1))
+        {
+            var currentTile = map[position.X, position.Y];
+            var nextTile = map[newX, newY];
+
+            if (nextTile == '.')
+            {
+                map[newX, newY] = currentTile;
+                map[position.X, position.Y] = '.';
+                return new Point(newX, newY);
+            }
+            else if (nextTile is '[' or ']' && movement is '<' or '>')
+            {
+                Move2(map, new Point(newX, newY), movement);
+                nextTile = map[newX, newY];
+                if (nextTile == '.')
+                {
+                    map[newX, newY] = currentTile;
+                    map[position.X, position.Y] = '.';
+                    return new Point(newX, newY);
+                }
+                return position;
+            }
+            else if (nextTile is '[' or ']' && movement is '^' or 'v')
+            {
+                var nextPosition = new Point(newX, newY);
+                var canMove = CanPushBox(map, nextPosition, (movement is '^') ? x => x - 1 : x => x + 1);
+                if (canMove)
+                {
+                    PushBox(map, nextPosition, movement);
+                    map[newX, newY] = currentTile;
+                    map[position.X, position.Y] = '.';
+                    return new Point(newX, newY);
+                }
+            }
+            else if (nextTile == '#')
+            {
+                // path blocked
+                return position;
+            }
+        }
+
+        return position;
+    }
+
+    public bool CanPushBox(char[,] map, Point position, Func<int, int> rowModifier)
+    {
+        var currentTile = map[position.X, position.Y];
+        if (currentTile == '.')
+        {
+            return true;
+        }
+        else if (currentTile == '#')
+        {
+            return false;
+        }
+        else
+        {
+            var currentTIly2Y = (currentTile == '[') ? position.Y + 1 : position.Y - 1;
+            return CanPushBox(map, position with { X = rowModifier(position.X) }, rowModifier)
+                && CanPushBox(map, new Point(rowModifier(position.X), currentTIly2Y), rowModifier);;
+        }
+    }
+
+    public void PushBox(char[,] map, Point position, char movement, Point? previousPosition = null)
+    {
+        var newX = position.X;
+
+        switch (movement)
+        {
+            case '^':
+                newX--;
+                break;
+            case 'v':
+                newX++;
+                break;
+        }
+
+        var currentTile = map[position.X, position.Y];
+        if (currentTile == '.')
+        {
+            var previousTile = map[previousPosition.Value.X, previousPosition.Value.Y];
+            map[position.X, position.Y] = previousTile;
+            map[previousPosition.Value.X, previousPosition.Value.Y] = '.';
+            return;
+        }
+        else if (currentTile == '#')
+        {
+            return;
+        }
+        else
+        {
+            var currentTile2Y = (currentTile == '[') ? position.Y + 1 : position.Y - 1;
+            PushBox(map, position with { X = newX }, movement, position);
+            PushBox(map, new Point(newX, currentTile2Y), movement, position with { Y = currentTile2Y });;
+
+            map[newX, position.Y] = currentTile;
+            map[newX, currentTile2Y] = (currentTile == '[') ? ']' : '[';
+
+            map[position.X, position.Y] = '.';
+            map[position.X, currentTile2Y] = '.';
+        }
     }
 }
